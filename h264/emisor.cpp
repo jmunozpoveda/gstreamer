bdca904d253823a9c3f60bb3920734b81a45f526
Your Name <you@example.com>
2016-02-11 00:55:44 +0100

    enlazando dinamicamente

--- h264/emisor.cpp
+++ h264/emisor.cpp
@@ -11,15 +11,15 @@
 #include <math.h>
 
 
-#define RTP_TX  5002
-#define RTCP_TX 5003
+#define RTP_TX  5000
+#define RTCP_TX 5001
 #define RTP_RX  5000
 #define RTCP_RX 5001
 
-#define RECV_FILENAME "recibidoA.wav"
+#define RECV_FILENAME "recA.mp4"
 
-#define AUDIO_CAPS "application/x-rtp,media=(string)audio,clock-rate=(int)8000,encoding-name=(string)AMR,encoding-params=(string)1,octet-align=(string)1" 
 
+#define VIDEO_CAPS "application/x-rtp,media=(string)video,clock-rate=(int)90000,encoding-name=(string)H264" 
 
 #define GST_ON_SENDER_TIMEOUT   "on-sender-timeout"
 #define GST_ON_TIMEOUT          "on-timeout"
@@ -32,7 +32,43 @@
 
   GMainLoop *loop_tx;
   GMainLoop *loop;
-
+static gboolean bus_call (GstBus *bus, GstMessage *msg, gpointer data) 
+	{ 
+	  GMainLoop *loop = (GMainLoop *) data; 
+	  switch (GST_MESSAGE_TYPE (msg)) { 
+	      case GST_MESSAGE_EOS: 
+	      g_print ("End of stream\n"); 
+	      g_main_loop_quit (loop); 
+	      break; 
+	    case GST_MESSAGE_ERROR: { 
+	gchar *debug; 
+	GError *error; 
+	gst_message_parse_error (msg, &error, &debug); 
+	g_free (debug); 
+	g_printerr ("Error: %s\n", error->message); 
+	g_error_free (error); 
+	g_main_loop_quit (loop); 
+	break; 
+	} 
+	default: 
+	break; 
+	} 
+	return TRUE; 
+	} 
+	static void 
+	on_pad_added (GstElement *element, 
+	GstPad *pad, 
+	gpointer data) 
+	{ 
+	GstPad *sinkpad; 
+	GstElement *decoder = (GstElement *) data; 
+	/* We can now link this pad with the vorbis-decoder sink pad */ 
+	g_print ("Dynamic pad created, linking demuxer/decoder\n"); 
+	sinkpad = gst_element_get_static_pad (decoder, "sink"); 
+	gst_pad_link (pad, sinkpad); 
+	gst_object_unref (sinkpad); 
+	} 
+ 
   
   static void print_source_stats (GObject * source)
 {
@@ -123,7 +159,7 @@ pad_added_cb (GstElement * rtpbin, GstPad * new_pad, GstElement * depay)
 
 static gboolean my_bus_callback (GstBus     *bus, GstMessage *message, gpointer    data)
 {
-  g_print ("Got %s message\n", GST_MESSAGE_TYPE_NAME (message));
+  //g_print ("Got %s message\n", GST_MESSAGE_TYPE_NAME (message));
 
   switch (GST_MESSAGE_TYPE (message)) {
     case GST_MESSAGE_ERROR: {
@@ -165,116 +201,70 @@ static void timeout_callback(GstElement* element, guint session, guint ssrc, gpo
 
 void *envia( void *ptr)
 {
-    GstElement *audiosrc_tx, *audioconv_tx, *audiores_tx, *audioenc_tx, *audiopay_tx, *wavparse_tx;
-  GstElement *rtpbin, *rtpsink_tx, *rtcpsink_tx, *rtcpsrc_tx;
-  GstElement *pipeline;
+ GMainLoop *loop; 
+	GstElement *pipeline, *source, *demuxer, *decoder, *conv, *sink; 
+	GstBus *bus; 
+	/* Initialisation */ 
+	gst_init (&argc, &argv); 
+	loop = g_main_loop_new (NULL, FALSE); 
+	/* Check input arguments */ 
+	if (argc != 2) { 
+	g_printerr ("Usage: %s <Ogg/Vorbis filename>\n", argv[0]); 
+	return -1; 
+	} 
+	/* Create gstreamer elements */ 
+	pipeline = gst_pipeline_new ("audio-player"); 
+	source = gst_element_factory_make ("filesrc", "file-source"); 
+	demuxer = gst_element_factory_make ("qtdemux", "demuxer"); 
+	decoder = gst_element_factory_make ("avdec_h264", "decoder"); 
+	sink = gst_element_factory_make ("udpsink", "video-output"); 
+	
+	  g_object_set (sink, "port", RTP_TX, "host", DEST_HOST, NULL);
   
-  GstPad *srcpad, *sinkpad;
-
-  /* the pipeline to hold everything */
-  pipeline = gst_pipeline_new (NULL);
-  g_assert (pipeline);
-
-    /* the audio capture and format conversion */
-  audiosrc_tx = gst_element_factory_make ("filesrc", "filesrc");
-  g_object_set(audiosrc_tx, "location", "audioA.wav", NULL);
-  
-  wavparse_tx = gst_element_factory_make ("wavparse", "wavparse_tx");
-  
-  audioconv_tx = gst_element_factory_make ("audioconvert", "audioconv_tx");
-  audiores_tx = gst_element_factory_make ("audioresample", "audiores_tx");
-  
-  /* the encoding and payloading */
-  audioenc_tx = gst_element_factory_make ("amrnbenc", "amrnbenc");
-  audiopay_tx = gst_element_factory_make ("rtpamrpay", "rtpamrpay");
-
-  /* add capture and payloading to the pipeline and link */
-  gst_bin_add_many (GST_BIN (pipeline), audiosrc_tx, wavparse_tx, audioconv_tx, audiores_tx,  audioenc_tx, audiopay_tx, NULL);
-
-  if (!gst_element_link_many (audiosrc_tx, wavparse_tx, audioconv_tx, audiores_tx, audioenc_tx, audiopay_tx, NULL)) {
-    g_error ("Failed to link audiosrc_tx, audioconv_tx, audiores_txample, "
-        "audio encoder and audio payloader");
-  }
-
-  /* the rtpbin element */
-  rtpbin = gst_element_factory_make ("rtpbin", "rtpbin");
-  g_assert (rtpbin);
-
-  gst_bin_add (GST_BIN (pipeline), rtpbin);
-
-  /* the udp sinks and source we will use for RTP and RTCP */
-  rtpsink_tx = gst_element_factory_make ("udpsink", "rtpsink_tx");
-  g_assert (rtpsink_tx);
-  g_object_set (rtpsink_tx, "port", RTP_TX, "host", DEST_HOST, NULL);
-
-  rtcpsink_tx = gst_element_factory_make ("udpsink", "rtcpsink_tx");
-  g_assert (rtcpsink_tx);
-  g_object_set (rtcpsink_tx, "port", RTCP_TX, "host", DEST_HOST, NULL);
-  g_object_set (rtcpsink_tx, "async", FALSE, "sync", FALSE, NULL);
-
-  rtcpsrc_tx = gst_element_factory_make ("udpsrc", "rtcpsrc_tx");
-  g_assert (rtcpsrc_tx);
-  g_object_set (rtcpsrc_tx, "port", RTCP_TX, NULL);
-
-  gst_bin_add_many (GST_BIN (pipeline), rtpsink_tx, rtcpsink_tx, rtcpsrc_tx, NULL);
-
-  /* now link all to the rtpbin, start by getting an RTP sinkpad for session 0 */
-  sinkpad = gst_element_get_request_pad (rtpbin, "send_rtp_sink_0");
-  srcpad = gst_element_get_static_pad (audiopay_tx, "src");
-  if (gst_pad_link (srcpad, sinkpad) != GST_PAD_LINK_OK)
-    g_error ("Failed to link audio payloader to rtpbin");
-  gst_object_unref (srcpad);
-
-  /* get the RTP srcpad that was created when we requested the sinkpad above and
-   * link it to the rtpsink_tx sinkpad*/
-  srcpad = gst_element_get_static_pad (rtpbin, "send_rtp_src_0");
-  sinkpad = gst_element_get_static_pad (rtpsink_tx, "sink");
-  if (gst_pad_link (srcpad, sinkpad) != GST_PAD_LINK_OK)
-    g_error ("Failed to link rtpbin to rtpsink_tx");
-  gst_object_unref (srcpad);
-  gst_object_unref (sinkpad);
-
-  /* get an RTCP srcpad for sending RTCP to the receiver */
-  srcpad = gst_element_get_request_pad (rtpbin, "send_rtcp_src_0");
-  sinkpad = gst_element_get_static_pad (rtcpsink_tx, "sink");
-  if (gst_pad_link (srcpad, sinkpad) != GST_PAD_LINK_OK)
-    g_error ("Failed to link rtpbin to rtcpsink_tx");
-  gst_object_unref (sinkpad);
-
-  /* we also want to receive RTCP, request an RTCP sinkpad for session 0 and
-   * link it to the srcpad of the udpsrc for RTCP */
-  srcpad = gst_element_get_static_pad (rtcpsrc_tx, "src");
-  sinkpad = gst_element_get_request_pad (rtpbin, "recv_rtcp_sink_0");
-  if (gst_pad_link (srcpad, sinkpad) != GST_PAD_LINK_OK)
-    g_error ("Failed to link rtcpsrc_tx to rtpbin");
-  gst_object_unref (srcpad);
-
-  
-    GstBus* bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
-  guint bus_watch_id = gst_bus_add_watch (bus, my_bus_callback, NULL);
-  gst_object_unref (bus);
-
-  
-  /* set the pipeline to playing */
-  g_print ("starting sender pipeline\n");
-  gst_element_set_state (pipeline, GST_STATE_PLAYING);
-
-  /* print stats every second */
-  g_timeout_add_seconds (1, (GSourceFunc) print_stats, rtpbin);
-
-  /* we need to run a GLib main loop_tx to get the messages */
-  loop_tx = g_main_loop_new (NULL, FALSE);
-  g_main_loop_run (loop_tx);
-
-  g_print ("stopping sender pipeline\n");
-  gst_element_set_state (pipeline, GST_STATE_NULL);
-  
+	
+	if (!pipeline || !source || !demuxer || !decoder || !sink) { 
+	g_printerr ("One element could not be created. Exiting.\n"); 
+	return -1; 
+	} 
+	/* Set up the pipeline */ 
+	/* we set the input filename to the source element */ 
+	g_object_set (G_OBJECT (source), "location", argv[1], NULL); 
+	/* we add a message handler */ 
+	bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline)); 
+	gst_bus_add_watch (bus, bus_call, loop); 
+	gst_object_unref (bus); 
+	/* we add all elements into the pipeline */ 
+	gst_bin_add_many (GST_BIN (pipeline), 
+	source, demuxer, decoder, sink, NULL); 
+	/* we link the elements together */ 
+	///gst_element_link (source, demuxer); 
+	//gst_element_link_many (decoder, sink, NULL); 
+	gst_element_link_many (source, demuxer, sink, NULL); 
+	g_signal_connect (demuxer, "pad-added", G_CALLBACK (on_pad_added), sink); 
+	/* note that the demuxer will be linked to the decoder dynamically. 
+	The reason is that Ogg may contain various streams (for example 
+	audio and video). The source pad(s) will be created at run time, 
+	by the demuxer when it detects the amount and nature of streams. 
+	Therefore we connect a callback function which will be executed 
+	when the "pad-added" is emitted.*/ 
+	/* Set the pipeline to "playing" state*/ 
+	g_print ("Now playing: %s\n", argv[1]); 
+	gst_element_set_state (pipeline, GST_STATE_PLAYING); 
+	/* Iterate */ 
+	g_print ("Running...\n"); 
+	g_main_loop_run (loop); 
+	/* Out of the main loop, clean up nicely */ 
+	g_print ("Returned, stopping playback\n"); 
+	gst_element_set_state (pipeline, GST_STATE_NULL); 
+	g_print ("Deleting pipeline\n"); 
+	gst_object_unref (GST_OBJECT (pipeline)); 
+	
 }
 
 void *recibe( void *ptr)
 {
-   GstElement *rtpbin, *rtpsrc_rx, *rtcpsrc_rx, *rtcpsink_rx;
-  GstElement *audiodepay_rx, *wavenc_rx, *audioconvert_rx, *audiosink_rx, *amrdecoder_rx;
+   GstElement *rtpbin, *rtpsrc_rx;
+  GstElement *rtph264depay, *mp4mux, *filesink, *h264parse;
 
   
   GstCaps *caps_rx;
@@ -283,98 +273,37 @@ void *recibe( void *ptr)
   GstPad *srcpad, *sinkpad;
 
 
-  /* the pipeline to hold everything */
   pipeline = gst_pipeline_new (NULL);
   g_assert (pipeline);
 
-  /* the udp src and source we will use for RTP and RTCP */
+  
+  h264parse = gst_element_factory_make ("h264parse", NULL);
+  
   rtpsrc_rx = gst_element_factory_make ("udpsrc", "rtpsrc_rx");
   g_assert (rtpsrc_rx);
   g_object_set (rtpsrc_rx, "port", RTP_RX, NULL);
-  /* we need to set caps_rx on the udpsrc for the RTP data */
-  caps_rx = gst_caps_from_string (AUDIO_CAPS);
+  
+  caps_rx = gst_caps_from_string (VIDEO_CAPS);
   g_object_set (rtpsrc_rx, "caps", caps_rx, NULL);
   gst_caps_unref (caps_rx);
 
-  rtcpsrc_rx = gst_element_factory_make ("udpsrc", "rtcpsrc_rx");
-  g_assert (rtcpsrc_rx);
-  g_object_set (rtcpsrc_rx, "port", RTCP_RX, NULL);
-
-  rtcpsink_rx = gst_element_factory_make ("udpsink", "rtcpsink_rx");
-  g_assert (rtcpsink_rx);
   
-  GSocket* socket = NULL;
-  g_object_get(rtcpsrc_rx,  "socket", &socket, NULL);
-  g_object_set(rtcpsink_rx, "socket", socket, NULL);
-
-    
-  /* no need for synchronisation or preroll on the RTCP sink */
-  g_object_set (rtcpsink_rx, "async", FALSE, "sync", FALSE, NULL);
-
-  gst_bin_add_many (GST_BIN (pipeline), rtpsrc_rx, rtcpsrc_rx, rtcpsink_rx, NULL);
-
-  /* the depayloading and decoding */
-  audiodepay_rx = gst_element_factory_make ("rtpamrdepay", "rtpamrdepay_tx");
-  amrdecoder_rx = gst_element_factory_make ("amrnbdec", "amrnbdec");
-  wavenc_rx = gst_element_factory_make   ("wavenc", "wavenc_rx");
-  audioconvert_rx = gst_element_factory_make  ("audioconvert", "audioconvert_rx");
-  audiosink_rx = gst_element_factory_make  ("filesink", "filesink");
+  
+  rtph264depay = gst_element_factory_make ("rtph264depay", "rtph264depay");
+  mp4mux = gst_element_factory_make  ("mp4mux", "mp4mux");
+  filesink = gst_element_factory_make  ("filesink", "filesink");
     
     
-  g_object_set(audiosink_rx, "location", RECV_FILENAME, NULL);
+  g_object_set(filesink, "location", RECV_FILENAME, NULL);
 
-  /* add depayloading and playback to the pipeline and link */
-  gst_bin_add_many (GST_BIN (pipeline), audiodepay_rx, amrdecoder_rx, audioconvert_rx,  wavenc_rx, audiosink_rx, NULL);
- 
-  res = gst_element_link_many (audiodepay_rx, amrdecoder_rx, audioconvert_rx,  wavenc_rx,  audiosink_rx, NULL);
+  
+  gst_bin_add_many (GST_BIN (pipeline), rtpsrc_rx, rtph264depay, h264parse, mp4mux,  filesink, NULL);
+  
+  res = gst_element_link_many (rtpsrc_rx, rtph264depay, h264parse, mp4mux, filesink, NULL);
   g_assert (res == TRUE);
 
-  /* the rtpbin element */
-  rtpbin = gst_element_factory_make ("rtpbin", "rtpbin");
-  g_assert (rtpbin);
 
-  gst_bin_add (GST_BIN (pipeline), rtpbin);
-
-  /* now link all to the rtpbin, start by getting an RTP sinkpad for session 0 */
-  srcpad = gst_element_get_static_pad (rtpsrc_rx, "src");
-  sinkpad = gst_element_get_request_pad (rtpbin, "recv_rtp_sink_1");
-  lres = gst_pad_link (srcpad, sinkpad);
-  g_assert (lres == GST_PAD_LINK_OK);
-  gst_object_unref (srcpad);
-
-  /* get an RTCP sinkpad in session 0 */
-  srcpad = gst_element_get_static_pad (rtcpsrc_rx, "src");
-  sinkpad = gst_element_get_request_pad (rtpbin, "recv_rtcp_sink_1");
-  lres = gst_pad_link (srcpad, sinkpad);
-  g_assert (lres == GST_PAD_LINK_OK);
-  gst_object_unref (srcpad);
-  gst_object_unref (sinkpad);
-
-  /* get an RTCP srcpad for sending RTCP back to the sender */
-  srcpad = gst_element_get_request_pad (rtpbin, "send_rtcp_src_1");
-  sinkpad = gst_element_get_static_pad (rtcpsink_rx, "sink");
-  lres = gst_pad_link (srcpad, sinkpad);
-  g_assert (lres == GST_PAD_LINK_OK);
-  gst_object_unref (sinkpad);
-
-  /* the RTP pad that we have to connect to the depayloader will be created
-   * dynamically so we connect to the pad-added signal, pass the depayloader as
-   * user_data so that we can link to it. */
-  g_signal_connect (rtpbin, "pad-added", G_CALLBACK (pad_added_cb), audiodepay_rx);
-    /* give some stats when we receive RTCP */
-  g_signal_connect (rtpbin, "on-ssrc-active", G_CALLBACK (on_ssrc_active_cb),     audiodepay_rx);
-    
-  g_signal_connect(rtpbin, "on-sender-timeout", G_CALLBACK(timeout_callback), (gpointer) GST_ON_SENDER_TIMEOUT);
-  g_signal_connect(rtpbin, "on-timeout",        G_CALLBACK(timeout_callback), (gpointer) GST_ON_TIMEOUT);
-  g_signal_connect(rtpbin, "on-bye-ssrc",       G_CALLBACK(timeout_callback), (gpointer) GST_ON_BYE_SSRC);
-  g_signal_connect(rtpbin, "on-bye-timeout",    G_CALLBACK(timeout_callback), (gpointer) GST_ON_BYE_TIME_OUT);
-
-  GstBus* bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
-  guint bus_watch_id = gst_bus_add_watch (bus, my_bus_callback, NULL);
-  gst_object_unref (bus);
-
-
-  /* set the pipeline to playing */
+  
   g_print ("starting receiver pipeline\n");
   gst_element_set_state (pipeline, GST_STATE_PLAYING);
   
@@ -389,6 +318,7 @@ void *recibe( void *ptr)
   return ;
   
 }
+
 int main (int argc, char *argv[])
 {
 
@@ -398,7 +328,7 @@ int main (int argc, char *argv[])
 
      pthread_t thread1, thread2;
 
-     const char *message1 = "Thread 1";
+      const char *message1 = "Thread 1";
      const char *message2 = "Thread 2";
      int  iret1, iret2;
 
@@ -406,8 +336,8 @@ int main (int argc, char *argv[])
      usleep(100000);
      iret2 = pthread_create( &thread2, NULL, envia, (void*) message2);
 
-     pthread_join( thread1, NULL);
-     pthread_join( thread2, NULL);
+      pthread_join( thread1, NULL);
+      pthread_join( thread2, NULL);
 
 
   return 0;
